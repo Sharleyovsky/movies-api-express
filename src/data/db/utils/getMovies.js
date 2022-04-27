825a8d14666d4815b9af29d176b6b65b922492ef
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 17:13:36 +0200

    Integrate getMovies with getRandomMovie

--- src/data/db/utils/getMovies.js
+++ src/data/db/utils/getMovies.js
@@ -1,19 +1,34 @@
 const readDbFile = require("../helpers/readDbFile");
+const lodash = require("lodash");
 const filterMoviesByDuration = require("../helpers/filterMoviesByDuration");
 const filterMoviesByGenres = require("../helpers/filterMoviesByGenres");
+const getRandomMovie = require("../helpers/getRandomMovie");
 
 const getMovies = async (queries = null) => {
   try {
     let { movies } = await readDbFile();
 
+    if (lodash.isEmpty(movies)) {
+      return [];
+    }
+
+    if (lodash.isEqual(queries, {}) || !queries) {
+      return getRandomMovie(movies);
+    }
+
     if (queries?.duration) {
-      const duration = +queries.duration;
-      movies = filterMoviesByDuration(movies, duration);
+      movies = filterMoviesByDuration(movies, +queries.duration);
+    }
+
+    if (
+      Object.keys(queries).includes("duration") &&
+      Object.keys(queries).length === 1
+    ) {
+      return getRandomMovie(movies);
     }
 
     if (queries?.genres) {
-      const genres = queries.genres;
-      movies = filterMoviesByGenres(movies, genres);
+      movies = filterMoviesByGenres(movies, queries.genres);
     }
 
     return movies;
