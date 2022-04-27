bc114e992577e6689f2f03f826a2de7e003b2982
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 19:53:44 +0200

    Add addMovie.js

---
+++ src/utils/addMovie.js
@@ -0,0 +1,16 @@
+const { writeFile } = require("fs").promises;
+const { dbPath } = require("../config/config");
+const readDbFile = require("../helpers/readDbFile");
+
+const addMovie = async (movie) => {
+  try {
+    const db = await readDbFile();
+    db.movies.push(movie);
+
+    await writeFile(dbPath, JSON.stringify(db));
+  } catch (error) {
+    console.log(error.toString());
+  }
+};
+
+module.exports = addMovie;
