79873fc9c4252d061342ebd4405f7b98d9eec1cb
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 18:49:44 +0200

    Remove uniq method

--- src/helpers/readDbFile.js
+++ src/helpers/readDbFile.js
@@ -1,13 +1,11 @@
 const { readFile } = require("fs").promises;
 const { dbPath } = require("../config/config");
-const lodash = require("lodash");
 
 const readDbFile = async () => {
   try {
     const data = await readFile(dbPath);
-    const movies = await JSON.parse(data);
 
-    return lodash.uniq(movies);
+    return await JSON.parse(data);
   } catch (error) {
     return error;
   }
