c0ca543364da20ab41f7dd6d54d9110ec3c111f0
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 22:30:23 +0200

    Add required keys error test

--- src/tests/routers/movies.test.js
+++ src/tests/routers/movies.test.js
@@ -72,6 +72,16 @@ const testMovies = [
     posterUrl:
       "http://ia.media-imdb.com/images/M/MV5BMTYzMzU4NDUwOF5BMl5BanBnXkFtZTcwMTM5MjA5Ng@@._V1_SX300.jpg",
   },
+  {
+    title: "A Separation 4",
+    year: 2019,
+    runtime: 145,
+    director: "Asghar Farhadi",
+    actors: "Peyman Moaadi, Leila Hatami, Sareh Bayat, Shahab Hosseini",
+    plot: "A married couple are faced with a difficult decision - to improve the life of their child by moving to another country or to stay in Iran and look after a deteriorating parent who has Alzheimer's disease.",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTYzMzU4NDUwOF5BMl5BanBnXkFtZTcwMTM5MjA5Ng@@._V1_SX300.jpg",
+  },
 ];
 
 afterAll(async () => {
@@ -129,5 +139,11 @@ describe("Test movies router endpoints", () => {
     const response = await request(app).post("/movies/add").send(testMovies[5]);
     await expect(response.statusCode).toBe(400);
     await expect(response.body.message).toBe(`Error: Invalid genres!`);
+  });
+
+  test("Should respond with code 400 and required keys error message", async () => {
+    const response = await request(app).post("/movies/add").send(testMovies[6]);
+    await expect(response.statusCode).toBe(400);
+    await expect(response.body.message).toBe(`Error: Missing required keys!`);
   });
 });
