1152e5c4fc9bcf3c8df684332828000c323c5f75
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 21:26:07 +0200

    Close server after finish tests

--- src/tests/routers/health.test.js
+++ src/tests/routers/health.test.js
@@ -1,9 +1,13 @@
 const request = require("supertest");
-const server = require("../../index");
+const { app, server } = require("../../index");
+
+afterAll(() => {
+  server.close();
+});
 
 describe("Test connection with server", () => {
   test("Should response with code 200", async () => {
-    const response = await request(server).get("/healthCheck");
+    const response = await request(app).get("/healthCheck");
     expect(response.statusCode).toBe(200);
   });
 });
