9fe69d24dc1885341258a518c68ff9e2e4873c34
Jakub Stolarski <gamernice223@gmail.com>
2022-04-27 18:00:41 +0200

    Add new post route

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const getMovies = require("../utils/getMovies");
+const movie = require("../middlewares/movie");
 
 const moviesRouter = new express.Router();
 const prefix = "/movies";
@@ -9,7 +10,20 @@ moviesRouter.get(prefix, async (req, res) => {
     const movies = await getMovies(req.body);
     res.send(movies);
   } catch (error) {
-    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+moviesRouter.post(`${prefix}/add`, movie, async (req, res) => {
+  try {
+    const movie = res.movie;
+
+    res.status(201).send({
+      message: `Movie ${res.movie.title} was added to the database!`,
+      movie,
+    });
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
